9cf3feb2d3aff32a1f3abcc52cd845ed78195f79
ZARIFMAC <uzbzarif@gmail.com>
2022-10-01 11:45:32 +0500

    remove contaners

--- lib/screen/listview_page.dart
+++ lib/screen/listview_page.dart
@@ -6,16 +6,6 @@ class listview_page extends StatelessWidget {
   }) : super(key: key);
   List<Widget> list = [
     ItemWidget(title: 'List Item 123'),
-    Container(
-      padding: EdgeInsets.all(8),
-      color: Colors.grey[200],
-      child: Text('List item 2'),
-    ),
-    Container(
-      padding: EdgeInsets.all(8),
-      color: Colors.grey[200],
-      child: Text('List item 1'),
-    )
   ];
   @override
   Widget build(BuildContext context) {
