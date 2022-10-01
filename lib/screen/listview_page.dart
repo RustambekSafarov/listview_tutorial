899cdca0e03c78b3cf636b5170cf0e959093c529
ZARIFMAC <uzbzarif@gmail.com>
2022-10-01 11:45:02 +0500

    Item Widget

--- lib/screen/listview_page.dart
+++ lib/screen/listview_page.dart
@@ -5,11 +5,7 @@ class listview_page extends StatelessWidget {
     Key? key,
   }) : super(key: key);
   List<Widget> list = [
-    Container(
-      padding: EdgeInsets.all(8),
-      color: Colors.grey[200],
-      child: Text('List item 1'),
-    ),
+    ItemWidget(title: 'List Item 123'),
     Container(
       padding: EdgeInsets.all(8),
       color: Colors.grey[200],
@@ -25,6 +21,20 @@ class listview_page extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: list,
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  String title = '';
+  ItemWidget({Key? key, String this.title = ''}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      color: Colors.grey[200],
+      child: Text(title),
     );
   }
 }
